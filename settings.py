5198886f306fed9e6beaffe798b391ae9eaace33
IvanBodnar <ibodnar@outlook.com>
2016-02-20 21:52:59 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 SECRET_KEY = '\x9a\x01\xacR\xe6\xe7]\x06\x04\x1a\x00\x0bX\xa6i\xf2wG\x88\xb0\x9cu\xd6\xb7'
 DEBUG = True
 DB_USERNAME = 'postgres'
-DB_PASSWORD = 'postgres'
+DB_PASSWORD = ''
 DB_HOST = 'localhost'
 RELEVAMIENTOS_DB_NAME = 'relevamientos'
 DB_URI = 'postgresql://%s:%s@%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOST, RELEVAMIENTOS_DB_NAME)
