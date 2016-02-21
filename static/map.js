322c5c8a55df22e51a31f12b9329b2c23ec69cb3
Ivan Bodnar <ivangbodnar@gmail.com>
2016-02-21 00:35:26 -0300

    Mapa prueba

---
+++ static/map.js
@@ -0,0 +1,9 @@
+
+var map = L.map('map').setView([-34.615715, -58.451204], 14);
+mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy: ' + mapLink,
+    maxZoom: 18
+    }).addTo(map);
