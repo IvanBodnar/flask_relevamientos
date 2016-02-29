d2ca836acc909c54c2453e0723f7bbaf93d5a970
Ivan Bodnar <ivangbodnar@gmail.com>
2016-02-29 19:39:50 -0300

    Prueba activar gps onload

--- static/loc.js
+++ static/loc.js
@@ -1,10 +1,21 @@
 var x = document.getElementById("demo");
 
+var dummy = function(){
+};
+
+function watchLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(dummy);
+    } else {
+        alert("Geolocalización no soportada en este navegador");
+    }
+}
+
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position);
+        navigator.geolocation.getCurrentPosition(position, dummy, {maximumAge:600000, timeout:5000, enableHighAccuracy: true});
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocalización no soportada en este navegador");
     }
 }
 
@@ -13,3 +24,7 @@ function position(position) {
     document.getElementById('Long').value = position.coords.longitude.toFixed(8);
     document.getElementById('Precision').value = position.coords.accuracy.toFixed(2);
 }
+
+window.onload = function() {
+    watchLocation();
+};
